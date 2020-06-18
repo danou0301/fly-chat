c00c74651bb4fb8d35a9c75c290db0bfd22ce8ff
danou0301 <dan.boujenah@gmail.com>
2020-06-18 18:07:18 +0200

    Add files via upload

---
+++ ChatBox/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace ChatBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textMsg = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listMsg = new System.Windows.Forms.ListBox();
+            this.listFiles = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.FlyChat = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.backgroundWorker3 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(615, 335);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.send_msg_Click);
+            // 
+            // textMsg
+            // 
+            this.textMsg.Location = new System.Drawing.Point(570, 295);
+            this.textMsg.Name = "textMsg";
+            this.textMsg.Size = new System.Drawing.Size(187, 20);
+            this.textMsg.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(588, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Send message";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(615, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "choose file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(610, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Send file";
+            // 
+            // listMsg
+            // 
+            this.listMsg.FormattingEnabled = true;
+            this.listMsg.Location = new System.Drawing.Point(23, 93);
+            this.listMsg.Name = "listMsg";
+            this.listMsg.Size = new System.Drawing.Size(324, 290);
+            this.listMsg.TabIndex = 7;
+            // 
+            // listFiles
+            // 
+            this.listFiles.FormattingEnabled = true;
+            this.listFiles.Location = new System.Drawing.Point(385, 93);
+            this.listFiles.Name = "listFiles";
+            this.listFiles.Size = new System.Drawing.Size(145, 290);
+            this.listFiles.TabIndex = 8;
+            this.listFiles.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listFiles_MouseDoubleClick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.panel1.Controls.Add(this.FlyChat);
+            this.panel1.Location = new System.Drawing.Point(-4, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(805, 43);
+            this.panel1.TabIndex = 9;
+            // 
+            // FlyChat
+            // 
+            this.FlyChat.AutoSize = true;
+            this.FlyChat.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FlyChat.ForeColor = System.Drawing.Color.Snow;
+            this.FlyChat.Location = new System.Drawing.Point(362, 7);
+            this.FlyChat.Name = "FlyChat";
+            this.FlyChat.Size = new System.Drawing.Size(93, 30);
+            this.FlyChat.TabIndex = 0;
+            this.FlyChat.Text = "Fly Chat";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Malgun Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Chat";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(381, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 21);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Files";
+            // 
+            // backgroundWorker3
+            // 
+            this.backgroundWorker3.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker3_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 427);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listFiles);
+            this.Controls.Add(this.listMsg);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textMsg);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textMsg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listMsg;
+        private System.Windows.Forms.ListBox listFiles;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label FlyChat;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ ChatBox/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace ChatBox
+{
+    public partial class Form1 : Form
+    {
+
+        private const int PORT = 80;
+        private const string IP_LOCAL = "127.0.0.1";
+        private const string SAVE_FILES_PATH = "C:\\Users\\dan\\Desktop\\user\\";
+        private bool isDownloadFile = false;
+
+        TcpClient serverSocket;
+        NetworkStream serverStream;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void send_msg_Click(object sender, EventArgs e)
+        {
+            if (textMsg.Text == "" || serverStream == null) // if send empty message not send to the server
+            {
+                return;
+            }
+            try
+            {
+                byte[] outStream = Encoding.ASCII.GetBytes(textMsg.Text);
+                serverStream.Write(outStream, 0, outStream.Length);
+                serverStream.Flush();
+                listMsg.Items.Add("- " + textMsg.Text);
+
+                textMsg.Text = "";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error connection interupt " + ex);
+                return;
+            }
+
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            serverSocket = new TcpClient();
+            
+            try
+            {
+                serverSocket.Connect(IP_LOCAL, PORT); // connect to server
+                serverStream = serverSocket.GetStream();
+                backgroundWorker3.RunWorkerAsync();
+                backgroundWorker1.WorkerSupportsCancellation = true;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("failed to connect to server " + ex);
+                return;
+            }
+
+        }
+
+
+        private void backgroundWorker3_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (serverSocket.Connected)
+            {
+                try
+                {
+                    if (!this.isDownloadFile) { // todo: if the user download file he doesn't listent the server, have to do on a thread
+                        byte[] inStream = new byte[10025];
+                        int bytesRead = serverStream.Read(inStream, 0, 10000);
+                        string getdata = Encoding.ASCII.GetString(inStream, 0, bytesRead);
+                        
+                        if (getdata.IndexOf("$-$: ") == 0) // get file name
+                        {
+                            string fileName = getdata.Substring(5);
+                            listFiles.Items.Add("> " + fileName);
+                        }
+                        else if (getdata.IndexOf("@-@: ") != 0) // get message, check that isn't a file message
+                        {
+                            listMsg.Items.Add("> " + getdata);
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error get from the server: " + ex);
+                    return;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "files (*.xml;*.json)|*.xml;*.json"; // show just compatible files
+            ofd.ShowDialog();  // open file dialog 
+
+            string filePath = ofd.FileName;
+            string safeFilePath = ofd.SafeFileName;  // filename without path
+            string file_extention = safeFilePath.Split('.').Last();
+            // check extention
+            if (file_extention != "json" && file_extention != "xml")
+            {
+                Console.WriteLine("Not Good Extention : " + file_extention);
+                return;
+            }
+
+            // send file to the server
+            byte[] fileNameByte = Encoding.ASCII.GetBytes("$-$: " + safeFilePath); // to do: not safe if in the chat write $
+            serverStream.Write(fileNameByte, 0, fileNameByte.Length);
+
+            byte[] data = File.ReadAllBytes(filePath);
+            // Build the package
+            byte[] dataLength = BitConverter.GetBytes(data.Length);
+            byte[] package = new byte[4 + data.Length];
+            dataLength.CopyTo(package, 0);
+            data.CopyTo(package, 4);
+
+            // Send to server
+            int bufferSize = 1024;
+            int bytesSent = 0;
+            int bytesLeft = package.Length;
+
+            while (bytesLeft > 0)
+            {
+                int nextPacketSize = (bytesLeft > bufferSize) ? bufferSize : bytesLeft;
+                serverStream.Write(package, bytesSent, nextPacketSize);
+                bytesSent += nextPacketSize;
+                bytesLeft -= nextPacketSize;
+            }
+        }
+
+
+        private  void saveFile(string file_name)
+        {
+            NetworkStream fileStream;
+            fileStream = serverSocket.GetStream();
+            // Read length of incoming data
+            byte[] length = new byte[4];
+            int bufferSize = 1024;
+            int allBytesRead = 0;
+
+            int bytesRead = fileStream.Read(length, 0, 4);
+            int dataLength = BitConverter.ToInt32(length, 0);
+
+            // Read the data
+            int bytesLeft = dataLength;
+            byte[] data = new byte[dataLength];
+
+            while (bytesLeft > 0)
+            {
+                //Console.WriteLine("bytes download: " + Convert.ToString(bytesLeft));
+                int nextPacketSize = (bytesLeft > bufferSize) ? bufferSize : bytesLeft;
+
+                bytesRead = fileStream.Read(data, allBytesRead, nextPacketSize);
+                allBytesRead += bytesRead;
+                bytesLeft -= bytesRead;
+
+            }
+            // Save file to the server
+            try
+            {
+                string path = SAVE_FILES_PATH + file_name;
+                File.WriteAllBytes(path, data);
+                Console.WriteLine("Finish !" + " Save in " + SAVE_FILES_PATH + file_name);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error save file: " + ex);
+            }
+
+        }
+
+
+        private void listFiles_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index = this.listFiles.IndexFromPoint(e.Location);
+            if (index != System.Windows.Forms.ListBox.NoMatches)
+            {
+                // download the file from the server
+                string file_name = listFiles.SelectedItem.ToString().Substring(2);
+                MessageBox.Show("Download: " + file_name);
+
+                // ask the server to send the file
+                byte[] outStream = Encoding.ASCII.GetBytes("@-@: " + file_name);
+                serverStream.Write(outStream, 0, outStream.Length);
+
+                // download the file
+                this.isDownloadFile = true;
+                saveFile(file_name);
+                this.isDownloadFile = false;
+
+                MessageBox.Show("Finish !");
+
+            }
+        }
+    }
+}
